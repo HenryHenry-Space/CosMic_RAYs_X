7559464890821f2387392a593fc7efac32ad24c5
Henry Xu <xhnfirst@outlook.com>
2022-02-08 23:20:45 +1100

    add ros env

---
+++ X_ROS/build/X_UR/UR5_driver/universal_robots/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/xhnfly/Cosmic_rays_X/X_ROS/src/X_UR/UR5_driver/universal_robots
+# Build directory: /home/xhnfly/Cosmic_rays_X/X_ROS/build/X_UR/UR5_driver/universal_robots
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ X_ROS/devel/share/robotiq_ft_sensor/cmake/robotiq_ft_sensor-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(robotiq_ft_sensor_MSG_INCLUDE_DIRS "/home/xhnfly/Cosmic_rays_X/X_ROS/src/X_UR/robotiq/robotiq_ft_sensor/msg")
+set(robotiq_ft_sensor_MSG_DEPENDENCIES std_msgs)
